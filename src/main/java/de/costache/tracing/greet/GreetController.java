ae868863b740505efb9aaebd8b3bc9d802795a6a
Theodor Costache <costache.theodor@gmail.com>
2020-05-27 23:42:32 +0200

    removed unused variable

--- src/main/java/de/costache/tracing/greet/GreetController.java
+++ src/main/java/de/costache/tracing/greet/GreetController.java
@@ -1,11 +1,9 @@
 package de.costache.tracing.greet;
 
-import brave.Tracer;
-import feign.Client;
-import feign.Feign;
-import feign.auth.BasicAuthRequestInterceptor;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicLong;
+import brave.Tracer;
+import feign.Client;
+import feign.Feign;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 
 @RestController
 public class GreetController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     private final Logger logger = LoggerFactory.getLogger(GreetController.class);
     private final Encoder encoder;
     private final Client client;
