7a12c53cba836d738cc7d030653e7bebed4d5369
Theodor Costache <costache.theodor@gmail.com>
2020-05-27 23:40:17 +0200

    better demo

--- start-win.sh
+++ start-win.sh
@@ -11,24 +11,17 @@ gradle clean build
 
 PIDS=()
 
-java -Xms512m -Xmx2G -XX:+UseG1GC -jar build/libs/open-tracing-demo-0.0.1-SNAPSHOT.jar > app.log & 
+NAME=SERVICE0 java -Xms512m -Xmx2G -XX:+UseG1GC -jar build/libs/open-tracing-demo-0.0.1-SNAPSHOT.jar > service0.log & 
 PIDS+=($!)
 
 jaeger-all-in-one --collector.zipkin.http-port=9411 2>jaeger.log &
 PIDS+=($!)
 
-for i in `seq 1 1`;
+for i in `seq 1 3`;
 do
-    PORT=808$i NAME=PROXY$i SERVICE_URL=http://localhost:808$(($i-1)) java -Xms1G -Xmx2G -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -jar build/libs/open-tracing-demo-0.0.1-SNAPSHOT.jar > proxy$i.log & 
+    PORT=808$i NAME=SERVICE$i SERVICE_URL=http://localhost:808$(($i-1)) java -Xms512m -Xmx2G -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -jar build/libs/open-tracing-demo-0.0.1-SNAPSHOT.jar > service$i.log & 
     PIDS+=($!)
 done   
-
-for i in `seq 2 3`;
-do
-    PORT=808$i NAME=PROXY$i SERVICE_URL=http://localhost:808$(($i-1)) java -Xms256m -Xmx2G -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -jar build/libs/open-tracing-demo-0.0.1-SNAPSHOT.jar > proxy$i.log &
-    PIDS+=($!)
-done
-
 
 read -n 1 -s -r -p "Press any key to continue"
 
