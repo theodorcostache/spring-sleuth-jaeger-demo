3fb3d92ecf02ca74bac34ebc214eb3b6d6469783
Theodor Costache <costache.theodor@gmail.com>
2020-07-14 22:42:43 +0200

    Simplified code

--- src/main/java/de/costache/tracing/greet/GreetController.java
+++ src/main/java/de/costache/tracing/greet/GreetController.java
@@ -1,60 +1,29 @@
 package de.costache.tracing.greet;
 
-import java.util.UUID;
-
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import brave.Tracer;
-import feign.Client;
-import feign.Feign;
-import feign.auth.BasicAuthRequestInterceptor;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
+import java.util.UUID;
 
 @RestController
 public class GreetController {
 
     private static final String template = "Hello, %s!";
     private final Logger logger = LoggerFactory.getLogger(GreetController.class);
-    private final Encoder encoder;
-    private final Client client;
-    private final Decoder decoder;
 
     private GreetingProxy feignClient;
-    private Tracer tracer;
 
     @Value("${upstream.url}")
     private String serviceUrl;
 
     @Autowired
-    public GreetController(
-            Decoder decoder, Encoder encoder, Client client, Tracer tracer) {
-
-        this.encoder = encoder;
-        this.client = client;
-        this.decoder = decoder;
-        this.tracer = tracer;
-    }
-
-    @PostConstruct
-    public void init() {
-        logger.info("Service url: {}", serviceUrl);
-        if (!StringUtils.isEmpty(serviceUrl)) {
-            this.feignClient = Feign.builder().client(client)
-                    .encoder(encoder)
-                    .decoder(decoder)
-                    .requestInterceptor(new BasicAuthRequestInterceptor("user", "user"))
-                    .target(GreetingProxy.class, serviceUrl);
-        }
+    public GreetController(GreetingProxy feignClient) {
+        this.feignClient = feignClient;
     }
 
     @RequestMapping("/greet")
@@ -67,11 +36,11 @@ public class GreetController {
         Thread.sleep(delay);
         generateRandomError();
 
-        if (feignClient != null) {
+        if (serviceUrl != null) {
             logger.info("Using service as proxy and delegating the call");
             return feignClient.greet(name, delay);
         } else {
-            logger.info("Proxy disabled, returning content directly", serviceUrl);
+            logger.info("Proxy disabled, returning content directly");
             return new Greet(UUID.randomUUID().toString(), String.format(template, name));
         }
     }
--- src/main/java/de/costache/tracing/greet/GreetingProxy.java
+++ src/main/java/de/costache/tracing/greet/GreetingProxy.java
@@ -1,13 +1,13 @@
 package de.costache.tracing.greet;
 
-import feign.Param;
-import feign.RequestLine;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@FeignClient(name="greeting", url = "${upstream.url}")
 public interface GreetingProxy {
 
-    @RequestLine("GET /greet?name={name}&delay={delay}")
-    Greet greet(@Param(value = "name") String name, @Param(value = "delay") long delay);
-
+    @RequestMapping(method = RequestMethod.GET, value = "/greet")
+    Greet greet(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "delay") long delay);
 }
